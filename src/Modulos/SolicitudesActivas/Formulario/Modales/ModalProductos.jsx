299353cf8b148bf0284ae63cd62f6a9da37cf568
EdilsonAlcantara <edilsona@bellon.com.do>
2025-01-23 17:59:28 -0400

    modificando el modal de agregar productos, agrandandolo y poniendolo a que sea seleccion multiple y que baje los productos seleccionados a las lineas

--- src/Modulos/SolicitudesActivas/Formulario/Modales/ModalProductos.jsx
+++ src/Modulos/SolicitudesActivas/Formulario/Modales/ModalProductos.jsx
@@ -1,44 +1,108 @@
-import React, { useState } from 'react';
-import { Modal } from 'react-bootstrap';
-import { useFormulario } from '../Controles/useFormulario';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { Button, Col, Container, Form, InputGroup, Modal, Row } from 'react-bootstrap';
+import * as Icon from 'react-bootstrap-icons';
 import AGGridTabla from '../../../../ComponentesGlobales/AGGridTabla';
-
+import { formatoMoneda } from '../../../../FuncionesGlobales';
+import { useFormulario } from '../Controles/useFormulario';
 
 export default function ModalProductos() {
 
-    const {state,dispatch} = useFormulario()
+    const { state, dispatch, pasarLineasDelModalAlDetalle } = useFormulario()
+    const AgGridReferencia = useRef(null);
+
+    useEffect(() => {
+        if (!state.modalAgregarProductos) {
+            return;
+        }
+    }, [state.modalAgregarProductos])
 
     const cerrarModal = () => {
         dispatch({ type: 'mostrarModalAgregarProductos', payload: { mostrar: false } })
     }
 
-    const [columnasSolicitudes] = useState([
-        { field: "id_producto", flex: 2 },
-        { field: "no_producto", flex: 1 },
-        { field: "descripcion", flex: 1 },
-        { field: "cantidad", flex: 1 },
-        { field: "costo", flex: 2 }
+    const obtenerReferenciaAgGrid = (ref) => {
+        AgGridReferencia.current = ref.current; // Save the reference
+    }
+
+    const filtrarListado = (filtro) => {
+        AgGridReferencia.current.api.setGridOption(
+            "quickFilterText",
+            filtro,
+        )
+    }
+
+    const [columnasListadoProductos] = useState([
+        { headerName: "Producto", field: "no", flex: 1, filter: true },
+        { headerName: "Descripcion", field: "descripcion", flex: 5, filter: true },
+        { headerName: "Unidad", field: "codigo_unidad_medida", flex: 2, filter: true },
+        { headerName: "Cantidad", field: "inventario", valueFormatter: (e) => formatoMoneda(e.value, 0, ''), flex: 2, filter: true },
+        { headerName: "Precio Unitario", field: "precio_unitario", valueFormatter: (e) => formatoMoneda(e.value, 1, '$'), flex: 2, filter: true }
     ]);
+
+    const seleccionarMultiplesProductos = (event) => {
+        const listado = event.api.getSelectedRows();
+        dispatch({ type: 'llenarProductosSeleccionados', payload: { productosSeleccionados: listado } })
+    };
+
+    const rowSelection = useMemo(() => {
+        return {
+            mode: 'multiRow'
+        };
+    }, []);
 
     return (
         <Modal
             show={state.modalAgregarProductos}
             onHide={cerrarModal}
-            size="lg"
+            size="xl"
             aria-labelledby="contained-modal-title-vcenter"
             centered
         >
+
             <Modal.Header closeButton>
                 <Modal.Title id="contained-modal-title-vcenter">
                     Productos
                 </Modal.Title>
             </Modal.Header>
+
             <Modal.Body>
-                <AGGridTabla
-                    rowData={state.listadoProductos}
-                    columnDefs={columnasSolicitudes}
-                />
+                <Container fluid>
+                    <Row>
+                        <Col lg="6" >
+                            <InputGroup className="mb-3">
+                                <InputGroup.Text id="basic-addon1"><Icon.Search /></InputGroup.Text>
+                                <Form.Control
+                                    type='text'
+                                    placeholder="Buscar productos..."
+                                    onChange={(e) => filtrarListado(e.target.value)}
+                                />
+                            </InputGroup>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <AGGridTabla
+                                obtenerReferenciaAgGrid={obtenerReferenciaAgGrid}
+                                colDefs={columnasListadoProductos}
+                                rowData={state.listadoProductos}
+                                rowSelection={rowSelection}
+                                // pagination={pagination}
+                                // paginationPageSize={paginationPageSize}
+                                // paginationPageSizeSelector={paginationPageSizeSelector}
+                                onSelectionChanged={seleccionarMultiplesProductos}
+                            />
+                        </Col>
+                    </Row>
+                </Container>
             </Modal.Body>
+            <Modal.Footer>
+                <Row>
+                    <Col>
+                        <Button variant="primary" title="Agregar productos seleccionados" onClick={() => pasarLineasDelModalAlDetalle()}> Agregar </Button>
+                    </Col>
+                </Row>
+            </Modal.Footer>
+
         </Modal>
     )
 }
