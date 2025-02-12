7846e62e082b9ba31a287e6703c8d02b942fd610
Edilson Alcantara <edilsona@bellon.com.do>
2025-02-12 15:18:01 -0400

    controlando las cartas de actividades que se muestran segun el perfil del usuario

--- src/Modulos/Inicio/Controles/InicioProveedor.jsx
+++ src/Modulos/Inicio/Controles/InicioProveedor.jsx
@@ -1,10 +1,10 @@
 import { createContext, useEffect, useReducer } from "react";
 import { useAlerta } from "../../../ControlesGlobales/Alertas/useAlerta";
 import { useCargandoInformacion } from "../../../ControlesGlobales/CargandoInformacion/useCargandoInformacion";
-import { obtenerDatos, obtenerDatosConId, obtenerDatosDelLocalStorage } from "../../../FuncionesGlobales";
+import { useNotas } from "../../../ControlesGlobales/Notas/useNotas";
+import { obtenerDatos, obtenerDatosDelLocalStorage } from "../../../FuncionesGlobales";
 import { EstadoInicialInicio } from "../Modelos/InicioModel";
 import { inicioReducer } from "./inicioReducer";
-import { useNotas } from "../../../ControlesGlobales/Notas/useNotas";
 
 export const inicioContexto = createContext(null)
 
@@ -17,25 +17,98 @@ export function InicioProveedor({ children }) {
 
     const [state, dispatch] = useReducer(inicioReducer, EstadoInicialInicio);
 
+    const cargarCartasPorEstado = () => {
+
+        const datosUsuarioLogueado = obtenerDatosDelLocalStorage(import.meta.env.VITE_APP_LOCALSTORAGE_NOMBRE_PERFIL_USUARIO);
+        const usuarioLogueado = datosUsuarioLogueado !== null;
+
+        let estados = [];
+        if (usuarioLogueado) {
+            switch (datosUsuarioLogueado.posicion_id) {
+                case 1: // 'Administrador':
+                    estados = [
+                        'Nueva',
+                        'Pendiente',
+                        'Aprobada',
+                        'Rechazada',
+                        'Entregada',
+                        'Confirmada',
+                        'Terminada',
+                    ]
+                    break;
+                case 2: // 'Director'
+                    estados = [
+                        'Pendiente',
+                    ]
+                    break;
+                case 3: // 'Gerente Tienda'
+                    estados = [
+                        'Pendiente',
+                        'Rechazada',
+                        'Aprobada',
+                    ]
+                    break;
+                case 4: // 'Gerente Area'
+                    estados = [
+                        'Pendiente',
+                        'Rechazada',
+                        'Aprobada',
+                    ]
+                    break;
+                case 5: // 'Solicitante'
+                    estados = [
+                        'Nueva',
+                        'Pendiente',
+                        'Aprobada',
+                        'Rechazada',
+                    ]
+                    break;
+                case 6: // 'Asistente Con. Inventario'
+                    estados = [
+                        'Confirmada',
+                    ]
+                    break;
+                case 7: // 'Asistente Contabilidad'
+                    estados = [
+                        'Terminada',
+                    ]
+                    break;
+                case 8: // 'Despachador'
+                    estados = [
+                        'Aprobada',
+                        'Confirmada',
+                    ]
+                    break;
+                default:
+                    estados = [];
+                    break;
+            }
+        }
+
+        return estados;
+    }
+
     const cargarActividades = async () => {
 
         dispatch({ type: 'limpiarContadoresActividades' })
         dispatchCargandoInformacion({ type: 'mostrarCargandoInformacion' })
         const { account } = obtenerDatosDelLocalStorage(import.meta.env.VITE_APP_LOCALSTORAGE_NOMBRE)
         const usuarioDestino = account.username;
-
+        const cartasActividad = cargarCartasPorEstado() ?? [];
         dispatch({ type: 'obtenerNombreUsuario', payload: { nombre: account.name } })
 
         let arrEstadosSolicitudes = [];
         await obtenerDatos('EstadoSolicitud', null)
             .then((res) => {
-                arrEstadosSolicitudes = res.data ?? [];
+                arrEstadosSolicitudes = res.data.filter(el => cartasActividad.includes(el.descripcion)) ?? [];
             }).catch(() => {
                 dispatchAlerta({ type: 'mostrarAlerta', payload: { mostrar: true, mensaje: 'Error, al intentar cargar Estados de solicitudes.', tipo: 'warning' } });
             });
 
+        console.log('arrEstadosSolicitudes =>', arrEstadosSolicitudes)
+
         for (const estadoSolicitud of arrEstadosSolicitudes) {
-            let baseRuta = import.meta.env.VITE_APP_BELLON_SOLICITUDES_NUEVAS
+            let baseRuta = import.meta.env.VITE_APP_BELLON_SOLICITUDES_NUEVAS;
             try {
                 const res = await obtenerDatos('Solicitud/Cantidad?estadoSolicitudId=' + estadoSolicitud.id_estado_solicitud, null);
                 dispatch({
