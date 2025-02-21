f2e54634a87c1846683810d021c58574a8e6580e
Edilson Alcantara <edilsona@bellon.com.do>
2025-02-21 18:05:33 -0400

    agregue nuevos parametros para la tabla de aggrid del historial de movimientos

--- src/Modulos/HistorialMovimientosSolicitud/Modelos/EstadoInicialHistorialMovimientosSolicitudes.js
+++ src/Modulos/HistorialMovimientosSolicitud/Modelos/EstadoInicialHistorialMovimientosSolicitudes.js
@@ -2,3 +2,8 @@ export const EstadoInicialMovimientosSolicitudes = {
     historialMovimientosSolicitudes: [],
     estadosSolicitud: []
 }
+
+export const pagination = true;
+export const paginationPageSize = 500;
+export const paginationPageSizeSelector = [200, 500, 1000];
+export const rowSelection = false
