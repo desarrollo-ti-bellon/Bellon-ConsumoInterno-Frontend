975904375a07dfd11ab05b667f751d06a3cd1109
Edilson Alcantara <edilsona@bellon.com.do>
2025-02-22 11:52:15 -0400

    cambiando de posicion las librerias

--- src/Modulos/HistorialMovimientosSolicitud/HistorialMovimientosSolicitudPrincipal.jsx
+++ src/Modulos/HistorialMovimientosSolicitud/HistorialMovimientosSolicitudPrincipal.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import ListadoLinks from '../../ComponentesGlobales/ListadoLinks'
-import HistorialMovimientosSolicitud from './Vistas/HistorialMovimientosSolicitud'
 import { HistorialMovimientosSolicitudesProveedor } from './Controles/HistorialMovimientosSolicitudesProveedor'
+import HistorialMovimientosSolicitud from './Vistas/HistorialMovimientosSolicitud'
 
 export default function HistorialMovimientosSolicitudPrincipal() {
     return (
