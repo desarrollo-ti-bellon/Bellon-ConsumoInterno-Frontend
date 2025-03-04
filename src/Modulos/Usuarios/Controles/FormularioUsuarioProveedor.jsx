5d4f8f70a218b94516a622c65fbcb651800b6a0f
Edilson Alcantara <edilsona@bellon.com.do>
2025-03-04 17:06:53 -0400

    bloquear el campo de nombre del usuario

--- src/Modulos/Usuarios/Controles/FormularioUsuarioProveedor.jsx
+++ src/Modulos/Usuarios/Controles/FormularioUsuarioProveedor.jsx
@@ -178,7 +178,7 @@ export const FormularioUsuarioProveedor = ({ children }) => {
                     ...state.inactivarCampos,
                     campo_id_usuario_ci: true,
                     campo_id_usuario: false,
-                    campo_nombre_usuario: false,
+                    campo_nombre_usuario: true,
                     campo_correo: false,
                     campo_codigo_sucursal: false,
                     campo_id_sucursal: false,
